61c9e2e556b127d99eb467946ae07d2a49791940
Alejandro Meza <aam79@duke.edu>
2019-06-05 15:30:35 -0400

    trying to fix repo

--- IssueClass.swift
+++
@@ -1,9 +0,0 @@
-//
-//  IssueClass.swift
-//  FixD
-//
-//  Created by Alejandro Meza on 6/5/19.
-//  Copyright © 2019 IssueAppTeam. All rights reserved.
-//
-
-import Foundation
--- README.md
+++
@@ -1,7 +0,0 @@
-Code+ Team focused on making an App to solve issues on the Duke Campus. 
-
-Members:
-Ann 
-Karen 
-Alejandro 
-Duc
