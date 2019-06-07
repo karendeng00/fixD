d1454ad8240be931d0a9b7c7f6afb06fe6096183
Karen Deng <karen.deng@duke.edu>
2019-06-07 11:19:09 -0400

    finished add issue storyboard

---
+++ FixD/FixD/IssueViewController.swift
@@ -0,0 +1,43 @@
+//
+//  IssueViewController.swift
+//  FixD
+//
+//  Created by Karen Deng on 6/7/19.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import UIKit
+
+class IssueViewController: UIViewController {
+
+    @IBOutlet weak var issueTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func cameraButton(_ sender: Any) {
+    }
+    
+    @IBAction func mapButton(_ sender: Any) {
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        
+        
+    }
+}
