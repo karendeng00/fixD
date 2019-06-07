046fcb17c343d57e9770c2ac15672bdde7bb6200
Alejandro Meza <aam79@duke.edu>
2019-06-07 10:12:21 -0400

    added intro screen

--- FixD/FixD/ViewController.swift
+++ FixD/FixD/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dukeImage: UIImageView!
+    @IBOutlet weak var fixdLogoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        dukeImage.image = UIImage(named:"duke.png")
+        fixdLogoImage.image = UIImage(named:"gears.png")
     }
 
 
