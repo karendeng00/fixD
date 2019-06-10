d0d46d076ec3eec09e153f3223f6e6d70f12fe37
Duc <dt135@duke.edu>
2019-06-10 15:59:04 -0400

    Issue Page and Controller

---
+++ FixD/FixD/IssuePageController.swift
@@ -0,0 +1,22 @@
+//
+//  IssuePageController.swift
+//  FixD
+//
+//  Created by Duc Tran on 6/10/19.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import UIKit
+
+class IssuePageController: UIViewController {
+
+    @IBOutlet weak var issueLabel: UILabel!
+    @IBOutlet weak var decriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
