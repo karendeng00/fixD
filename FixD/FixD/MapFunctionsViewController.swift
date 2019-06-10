ed2f2261d6c52e3ea907229ecc3d53fd65f32bb8
Alejandro Meza <aam79@duke.edu>
2019-06-10 15:06:43 -0400

    map now zooms on current location, 2000m

--- FixD/FixD/MapFunctionsViewController.swift
+++ FixD/FixD/MapFunctionsViewController.swift
@@ -13,13 +13,31 @@ class MapFunctionsViewController: UIViewController {
     
     @IBOutlet weak var myMapView: MKMapView!
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        //Sets up the Current Location
+        setUpCurrentLocation()
     }
+    
+    private func setUpCurrentLocation() {
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Check for Location Services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        //Zoom to user location
+        if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 2000, longitudinalMeters: 2000)
+            myMapView.setRegion(viewRegion, animated: false)
+        }
+    }
+}
 
     
     
@@ -33,4 +51,5 @@ class MapFunctionsViewController: UIViewController {
     }
     */
 
-}
+
+
