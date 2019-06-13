a88f2bb3922384b9583814d749d877f70cd8babe
Ann Bailey <ann.bailey@duke.edu>
2019-06-13 13:42:46 -0400

    three drop down menus done

--- FixD/FixD/NewIssueOITViewController.swift
+++ FixD/FixD/NewIssueOITViewController.swift
@@ -14,15 +14,18 @@ class NewIssueOITViewController: UIViewController {
     @IBOutlet weak var buildingTV: UITableView!
     @IBOutlet weak var floorButton: UIButton!
     @IBOutlet weak var floorTV: UITableView!
-    
+    @IBOutlet weak var roomButton: UIButton!
+    @IBOutlet weak var roomTV: UITableView!
     
     let buildingList = ["Allen", "Brodhead Center", "Bryan Center", "Craven", "Few", "Perkins"]
     let floorList = ["1", "2", "3"]
+    let roomList = ["a", "b", "c"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         buildingTV.isHidden = true
         floorTV.isHidden = true
+        roomTV.isHidden = true
     }
     
     @IBAction func buildingButtonDrop(_ sender: Any) {
@@ -40,6 +43,15 @@ class NewIssueOITViewController: UIViewController {
         }
         else {
             floorAnimate(toggle: false)
+        }
+    }
+    
+    @IBAction func roomButtonDrop(_ sender: Any) {
+        if roomTV.isHidden {
+            roomAnimate(toggle: true)
+        }
+        else {
+            roomAnimate(toggle: false)
         }
     }
     
@@ -68,6 +80,19 @@ class NewIssueOITViewController: UIViewController {
             }
         }
     }
+    
+    func roomAnimate(toggle: Bool) {
+        if toggle {
+            UIView.animate(withDuration: 0.3) {
+                self.roomTV.isHidden = false
+            }
+        }
+        else {
+            UIView.animate(withDuration: 0.3) {
+                self.roomTV.isHidden = true
+            }
+        }
+    }
 }
 
 extension NewIssueOITViewController: UITableViewDelegate, UITableViewDataSource {
@@ -77,6 +102,9 @@ extension NewIssueOITViewController: UITableViewDelegate, UITableViewDataSource 
         }
         else if tableView == floorTV {
             return floorList.count
+        }
+        else if tableView == roomTV {
+            return roomList.count
         }
         return 0
     }
@@ -93,6 +121,11 @@ extension NewIssueOITViewController: UITableViewDelegate, UITableViewDataSource 
             cell.textLabel?.text = floorList[indexPath.row]
             returnCell = cell
         }
+        else if tableView == roomTV {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RoomCell", for: indexPath)
+            cell.textLabel?.text = roomList[indexPath.row]
+            returnCell = cell
+        }
         return returnCell
     }
     
@@ -104,6 +137,10 @@ extension NewIssueOITViewController: UITableViewDelegate, UITableViewDataSource 
         if tableView == floorTV {
             floorButton.setTitle("\(floorList[indexPath.row])", for: .normal)
             floorAnimate(toggle: false)
+        }
+        if tableView == roomTV {
+            roomButton.setTitle("\(roomList[indexPath.row])", for: .normal)
+            roomAnimate(toggle: false)
         }
     }
 }
