77ff74884956c47a45d27d53a1c44bf8337f8042
Ann Bailey <ann.bailey@duke.edu>
2019-06-25 15:49:44 -0400

    submission page added

---
+++ FixD/FixD/newissue/SubmissionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SubmissionViewController.swift
+//  FixD
+//
+//  Created by Ann Bailey on 6/25/19.
+//  Copyright © 2019 Duke. All rights reserved.
+//
+
+import UIKit
+
+class SubmissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
