608fb82506c03a05b8537724bf5d99a3e6a91dc7
Karen Deng <karen.deng@duke.edu>
2019-06-27 11:05:13 -0400

    pushed changes to make nice view show up

--- FixD/FixD/feed/IssuePageController.swift
+++ FixD/FixD/feed/IssuePageController.swift
@@ -33,7 +33,8 @@ class IssuePageController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var comView: UIView!
     
     
-
+    @IBOutlet var views: [UIView]!
+    
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var favButton: UIButton!
     @IBOutlet weak var comButton: UIButton!
@@ -53,6 +54,9 @@ class IssuePageController: UIViewController, UITableViewDelegate, UITableViewDat
     var issueID:Int = 0
     var issue:IssueClass!
     
+    let white = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255/255.0, alpha: 1.0)
+    let granite = UIColor(red: 181/255.0, green: 181/255.0, blue: 181/255.0, alpha: 0.5)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         commentTextField.delegate = self
@@ -60,21 +64,88 @@ class IssuePageController: UIViewController, UITableViewDelegate, UITableViewDat
         commentView.dataSource = self
         loadIssue()
         
-        let tapLike = UITapGestureRecognizer(target: self, action: #selector(like(_:)))
-        likeView.addGestureRecognizer(tapLike)
+        for v in views {
+            v.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 1.0, 0.8])
+            v.layer.masksToBounds = false
+            v.layer.cornerRadius = 2.0
+            v.layer.shadowOffset = CGSize(width: -1.5, height: 1.5)
+            v.layer.shadowOpacity = 0.22
+        }
         
-        let tapFav = UITapGestureRecognizer(target: self, action: #selector(favorite(_:)))
-        favView.addGestureRecognizer(tapFav)
+        issueImage.layer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 1.0, 0.8])
+        issueImage.layer.masksToBounds = false
+        issueImage.layer.cornerRadius = 2.0
+        issueImage.layer.shadowOffset = CGSize(width: -1, height: 1)
+        issueImage.layer.shadowOpacity = 0.2
+        
+
+        let longLike = UILongPressGestureRecognizer(target: self, action: #selector(longL(_:)))
+        longLike.minimumPressDuration = 0
+        likeView.addGestureRecognizer(longLike)
+        
+        let longFav = UILongPressGestureRecognizer(target: self, action: #selector(longF(_:)))
+        longFav.minimumPressDuration = 0
+        favView.addGestureRecognizer(longFav)
+        
+        let longCom = UILongPressGestureRecognizer(target: self, action: #selector(longC(_:)))
+        longCom.minimumPressDuration = 0
+        comView.addGestureRecognizer(longCom)
+        
+    }
+    
+    @objc func longL(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .ended {
+            commentView.backgroundColor = white
+            commentView.layer.shadowOffset = CGSize(width: -1, height: 1)
+            issue.addUpVote()
+            
+            if (issue.getUpVoteState()){
+                likeButton.setImage(UIImage(named: "filled heart"), for: .normal)
+            } else {
+                likeButton.setImage(UIImage(named: "heart-1"), for: .normal)
+            }
+        }
+        else {
+            
+            likeView.backgroundColor = granite
+            likeView.layer.shadowOffset = CGSize(width: -10, height: 10)
+        }
+        
+    }
+    
+    @objc func longF(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .ended {
+            favView.backgroundColor = white
+            favView.layer.shadowOffset = CGSize(width: -1, height: 1)
+            
+            issue.addFavorites()
+            if (issue.getFavoritesState()){
+                favButton.setImage(UIImage(named: "filled star"), for: .normal)
+            }else {
+                favButton.setImage(UIImage(named: "star"), for: .normal)
+            }
+        }
+        else {
+            favView.backgroundColor = granite
+            favView.layer.shadowOffset = CGSize(width: -10, height: 10)
+        }
+        
     }
     
     
-    @IBAction func liked(_ sender: Any) {
-        issue.addUpVote()
-        if (issue.getUpVoteState()){
-            likeButton.setImage(UIImage(named: "filled heart"), for: .normal)
-        }else {
-            likeButton.setImage(UIImage(named: "heart-1"), for: .normal)
+    @objc func longC(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .ended {
+            comView.backgroundColor = white
+            comView.layer.shadowOffset = CGSize(width: -1, height: 1)
         }
+        else {
+            comView.backgroundColor = granite
+            comView.layer.shadowOffset = CGSize(width: -10, height: 10)
+        }
+        
     }
     
     
