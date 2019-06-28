98b114079a365d33f579d784f4390e82b3c1edf7
Duc <dt135@duke.edu>
2019-06-28 14:12:17 -0400

    sliding keyboard and user screen

--- FixD/FixD/login/LoginScreenController.swift
+++ FixD/FixD/login/LoginScreenController.swift
@@ -12,10 +12,34 @@ class LoginScreenController: UIViewController {
     
     @IBOutlet weak var usernameTextBox: UITextField!
     
+    private var myUserAccount: FixDAccount?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        usernameTextBox.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGesture)
     }
 
+    @objc func handleTap(){
+        view.endEditing(true)
+    }
+    
+    func createAccount(name: String) {
+        myUserAccount = FixDAccount(name: name)
+    }
+    
+    @IBAction func submit(_ sender: UIButton) {
+        view.endEditing(true)
+        performSegue(withIdentifier: "LoginButton", sender: sender)
+    }
 }
 
+extension LoginScreenController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
