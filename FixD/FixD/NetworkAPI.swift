8400bb06b4db98595953d8ff66e8c35b736f967e
Alejandro Meza <aam79@duke.edu>
2019-07-23 11:40:01 -0400

    fixed comments and lauomments and launchscreen logo

--- FixD/FixD/NetworkAPI.swift
+++ FixD/FixD/NetworkAPI.swift
@@ -13,6 +13,8 @@ import Apollo
 
 class NetworkAPI {
     
+    let myUser = UserAccount.shared
+    
     // This method fetches information from IDMS to populate user. If the user already exists
     // in the system, then the user is loaded. Otherwise, a new user is created with the
     // information loaded from IDMS. The method instantiantes a singleton UserAccount.
@@ -192,7 +194,7 @@ class NetworkAPI {
                     var listOfComments:[CommentsClass] = []
                     if let comments = issue.comments {
                         for comment in comments {
-                            listOfComments.append(CommentsClass(body: comment.body ?? "", image: UIImage(), userId: comment.userId, issueId: comment.issueId, name: issue.user.name!, user_image: issue.user.picture ?? ""))
+                            listOfComments.append(CommentsClass(body: comment.body ?? "", image: UIImage(), userId: comment.userId, issueId: comment.issueId, name: comment.user.name!, user_image: comment.user.picture ?? ""))
                         }
                     }
                     myIssueList.append(IssueClass(
@@ -226,7 +228,7 @@ class NetworkAPI {
                 var listOfComments:[CommentsClass] = []
                 if let comments = i.comments {
                     for comment in comments {
-                        listOfComments.append(CommentsClass(body: comment.body ?? "", image: UIImage(), userId: comment.userId, issueId: comment.issueId, name: i.user.name!, user_image: i.user.picture ?? ""))
+                        listOfComments.append(CommentsClass(body: comment.body ?? "", image: UIImage(), userId: comment.userId, issueId: comment.issueId, name: comment.user.name!, user_image: comment.user.picture ?? ""))
                     }
                 }
                 let issue = IssueClass(issueID: Int(i.id)!,
@@ -264,7 +266,7 @@ class NetworkAPI {
     }
     
     func buildIssue(issue :IssueClass) {
-        Apollo().getClient().perform(mutation: CreateIssueMutation(description: issue.getDescription(), image: issue.getIssueImage(), location: issue.getLocation(), userId: 1, title: issue.getTitle(), type: issue.getType(), likes: 0, favorites: 0, email: issue.getEmail(), phone: issue.getPhone(), alternatePhone: issue.getAltPhone(), group: "", urgency: issue.getUrgency(), sensitiveInfo: issue.getSensitiveInfo(), campus: issue.getCampus(), area: issue.getArea(), specificLocation: issue.getSpecificLocation(), roomNumber: issue.getRoom(), serviceAnimal: issue.getAnimal(), impact: issue.getImpact(), yourBuilding: issue.getBuildingFacilities(), yourFloor: issue.getFloorFacilities(), yourRoom: issue.getRoomFacilities(), requestType: issue.getRequestFor(), issueBuilding: issue.getBuildingService(), issueFloor: issue.getFloorService(), issueRoom: issue.getRoomService(), serviceType: issue.getServiceType(), fundCode: issue.getFundCode(), topic: "", name: issue.getName())) { (result, error) in
+        Apollo().getClient().perform(mutation: CreateIssueMutation(description: issue.getDescription(), image: issue.getIssueImage(), location: issue.getLocation(), userId: myUser.getUserId() , title: issue.getTitle(), type: issue.getType(), likes: 0, favorites: 0, email: issue.getEmail(), phone: issue.getPhone(), alternatePhone: issue.getAltPhone(), group: "", urgency: issue.getUrgency(), sensitiveInfo: issue.getSensitiveInfo(), campus: issue.getCampus(), area: issue.getArea(), specificLocation: issue.getSpecificLocation(), roomNumber: issue.getRoom(), serviceAnimal: issue.getAnimal(), impact: issue.getImpact(), yourBuilding: issue.getBuildingFacilities(), yourFloor: issue.getFloorFacilities(), yourRoom: issue.getRoomFacilities(), requestType: issue.getRequestFor(), issueBuilding: issue.getBuildingService(), issueFloor: issue.getFloorService(), issueRoom: issue.getRoomService(), serviceType: issue.getServiceType(), fundCode: issue.getFundCode(), topic: "", name: issue.getName())) { (result, error) in
             if let err = error as? GraphQLHTTPResponseError {
                 print("Error: ", err.response.statusCode)
                 print("Error Submitting Issue")
